300c34e56b2043fc1806f32d6dbdc4d98f66c884
Hogunz <cjdc1414@gmail.com>
2023-11-17 14:31:15 +0800

    11/17/2023

---
+++ app/Models/BSCSTestimonial.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BSCSTestimonial extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2023_11_17_060515_create_b_s_i_t_testimonials_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('b_s_i_t_testimonials', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('b_s_i_t_testimonials');
+    }
+};
