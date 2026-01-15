954fe830e85142b5412a67a1f9722813f03f68ae
Hogunz <cjdc1414@gmail.com>
2026-01-15 11:26:37 +0800

    AppServiceProvider update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        //asd
         if($this->app->environment('production')) {
             URL::forceScheme('https');
         }
